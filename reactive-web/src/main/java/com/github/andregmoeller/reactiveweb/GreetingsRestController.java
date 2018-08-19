d6a6b50990fcb291c9b14d73c1a821f4124cf19d
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2018-08-19 19:23:37 +0200

    Changes endpoint so that it publishes a thousand records

--- reactive-web/src/main/java/com/github/andregmoeller/reactiveweb/GreetingsRestController.java
+++ reactive-web/src/main/java/com/github/andregmoeller/reactiveweb/GreetingsRestController.java
@@ -3,12 +3,15 @@ package com.github.andregmoeller.reactiveweb;
 import org.reactivestreams.Publisher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class GreetingsRestController {
     @GetMapping("/greetings")
     Publisher<Greeting> greetings() {
-        return Mono.just(new Greeting("Hello, world!"));
+        Flux<Greeting> greetingFlux = Flux
+                .<Greeting>generate(sink -> sink.next(new Greeting("Hello, world!")))
+                .take(1000);
+        return greetingFlux;
     }
 }
