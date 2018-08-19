872bfd17bfd43a5f1e25b943585966b1135c5219
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2018-08-19 22:46:32 +0200

    Converts the sse endpoint to a RequestPredicate / HandlerFunction

--- reactive-web/src/main/java/com/github/andregmoeller/reactiveweb/RouterConfiguration.java
+++ reactive-web/src/main/java/com/github/andregmoeller/reactiveweb/RouterConfiguration.java
@@ -1,9 +1,14 @@
 package com.github.andregmoeller.reactiveweb;
 
+import org.reactivestreams.Publisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.Instant;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -12,8 +17,14 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class RouterConfiguration {
     @Bean
     RouterFunction<ServerResponse> routes() {
-        return route(GET("/greetings"),
-                serverRequest -> ServerResponse.ok().body(Flux.just("Hello, world!"), String.class))
-                .andRoute(GET("/hi"), serverRequest ->  ServerResponse.ok().body(Flux.just("Hi"), String.class));
+        Publisher<Greeting> delayPublisher = Flux
+                .<Greeting>generate(sink -> sink.next(new Greeting("Hello, world @ " + Instant.now().toString())))
+                .delayElements(Duration.ofSeconds(1));
+
+        return route(GET("/greetings"), r -> ServerResponse.ok().body(Flux.just("Hello, world!"), String.class))
+                .andRoute(GET("/hi"), r ->  ServerResponse.ok().body(Flux.just("Hi"), String.class))
+                .andRoute(GET("/sse"), r -> ServerResponse.ok()
+                        .contentType(MediaType.TEXT_EVENT_STREAM)
+                        .body(delayPublisher, Greeting.class));
     }
 }
